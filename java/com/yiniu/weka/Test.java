bcd5f73335ae6dd134409cc220a609472c0662c1
zhangkunyong <zhangkunyong@zhangkunyong.yiniu.com>
2016-12-14 17:27:28 +0800

    测试

---
+++ java/com/yiniu/weka/Test.java
@@ -0,0 +1,12 @@
+package com.yiniu.weka;
+
+/**
+ * Author zhangkunyong
+ * Date 2016/12/14.
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("**********************");
+    }
+}
